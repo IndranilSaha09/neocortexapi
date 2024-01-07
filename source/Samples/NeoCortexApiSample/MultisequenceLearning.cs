0ef567836fcd5e1abe3b53f99a33654cc5218b9d
unknown <riswanbasha26@gmail.com>
2024-01-07 19:58:49 +0100

    PPTX Commit update

--- source/Samples/NeoCortexApiSample/MultisequenceLearning.cs
+++ source/Samples/NeoCortexApiSample/MultisequenceLearning.cs
@@ -136,7 +136,7 @@ namespace NeoCortexApiSample
 
             var lastPredictedValues = new List<string>(new string[] { "0" });
 
-            int maxCycles = 100;
+            int maxCycles = 200;
 
             //
             // Training SP to get stable. New-born stage.
