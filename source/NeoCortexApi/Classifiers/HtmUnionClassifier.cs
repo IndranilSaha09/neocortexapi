4d6151981c39c2fe51bd9eaee839f6172df1b412
unknown <riswanbasha26@gmail.com>
2024-01-12 10:01:02 +0100

    Code Clean

--- source/NeoCortexApi/Classifiers/HtmUnionClassifier.cs
+++ source/NeoCortexApi/Classifiers/HtmUnionClassifier.cs
@@ -35,50 +35,6 @@ namespace NeoCortexApi.Classifiers
             throw new System.NotImplementedException();
         }
 
-        //private Dictionary<TIN, int[]> activeMap = new Dictionary<TIN, int[]>();
-
-        //public void Learn(TIN input, Cell[] activeCells, bool learn)
-        //{
-        //    if (learn == true)
-        //    {
-        //        int[] unionArray;
-        //        int[] cellAsInt = new int[activeCells.Length];
-        //        for (int i = 0; i < activeCells.Length; i++)
-        //        {
-        //            cellAsInt[i] = activeCells[i].Index;
-        //        }
-        //        if (!activeMap.TryGetValue(input, out unionArray))
-        //        {
-        //            this.activeMap.Add(input, cellAsInt);
-        //            return; // or whatever you want to do
-        //        }
-        //        else
-        //        {
-        //            this.activeMap[input] = GetUnionArr(cellAsInt, activeMap[input]);
-        //        }
-        //    }
-        //}
-
-        //public String Inference(Cell[] activeCells)
-        //{
-        //    int result = 0;
-        //    String charOutput = null;
-        //    int[] arr = new int[activeCells.Length];
-        //    for (int i = 0; i < activeCells.Length; i++)
-        //    {
-        //        arr[i] = activeCells[i].Index;
-        //    }
-        //    foreach (var key in activeMap.Keys)
-        //    {
-        //        if (result < predictNextValue(arr, activeMap[key]))
-        //        {
-        //            result = predictNextValue(arr, activeMap[key]);
-        //            charOutput = key as String;
-        //        }
-        //    }
-        //    return charOutput;
-        //}
-
         public void Learn(TIN input, Cell[] activeCells, bool learn)
         {
             if (learn == true)
